a5440391671581421916fd7e6c77a4b7ca6240cd
William <wms274@nyu.edu>
2022-10-23 14:35:47 -0400

    fixed promotion logic

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import { Chess } from "chess.js";
 function App() {
   const [game, setGame] = useState(new Chess())
 
-  // let clone = Object.assign(Object.create(Object.getPrototypeOf(game)), game)
-
   function makeAMove(move) {
     console.log(move)
     const gameCopy = Object.assign(Object.create(Object.getPrototypeOf(game)), game);
@@ -17,12 +15,21 @@ function App() {
     return result; // null if the move was illegal, the move object if the move was legal
   }
 
-  function onDrop(sourceSquare, targetSquare) {
-    const move = makeAMove({
-      from: sourceSquare,
-      to: targetSquare
-      // promotion: 'q' // always promote to a queen for example simplicity
-    });
+  function onDrop(sourceSquare, targetSquare, piece) {
+    console.log(sourceSquare[1], targetSquare[1], piece[1].toLowerCase)
+    let move;
+    if ((sourceSquare[1] === '7' && piece[1].toLowerCase() === 'p' && targetSquare[1] === '8') || (sourceSquare[1] === '2' && piece[1].toLowerCase() === 'p' && targetSquare[1] === '1')) {
+      move = makeAMove({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: 'q'
+      });
+    } else {
+      move = makeAMove({
+        from: sourceSquare,
+        to: targetSquare
+      });
+    }
 
     console.log(move)
 
