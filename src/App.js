a9042e5e1855225cc3de4ae2e09b6e45863955aa
William <wms274@nyu.edu>
2022-10-23 14:12:16 -0400

    basic logic working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import { Chessboard } from "react-chessboard";
+import { Chess } from "chess.js";
 
 function App() {
+  const [game, setGame] = useState(new Chess())
+
+  // let clone = Object.assign(Object.create(Object.getPrototypeOf(game)), game)
+
+  function makeAMove(move) {
+    console.log(move)
+    const gameCopy = Object.assign(Object.create(Object.getPrototypeOf(game)), game);
+    const result = gameCopy.move(move);
+    setGame(gameCopy);
+
+    return result; // null if the move was illegal, the move object if the move was legal
+  }
+
+  function onDrop(sourceSquare, targetSquare) {
+    const move = makeAMove({
+      from: sourceSquare,
+      to: targetSquare
+      // promotion: 'q' // always promote to a queen for example simplicity
+    });
+
+    console.log(move)
+
+    // illegal move
+    if (move === null) return false;
+    
+    return true;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Chessboard id="BasicBoard" position={game.fen()} onPieceDrop={onDrop} />
     </div>
   );
 }
